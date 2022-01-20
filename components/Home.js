10cb5c683788a5556d72f9170a64f185f0f8587c
Fustigate <ianchang891@gmail.com>
2022-01-20 08:47:13 +0800

    Finished most of Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react"
-import { StyleSheet, View, Image, ScrollView, Dimensions, Text } from 'react-native'
+import { StyleSheet, View, Image, ScrollView, Dimensions, Text, TouchableOpacity } from 'react-native'
 import { Montez_400Regular } from '@expo-google-fonts/montez'
 import { useFonts } from "expo-font"
 import AppLoading from "expo-app-loading"
 import { Title } from 'react-native-paper'
+import { Roboto_400Regular } from '@expo-google-fonts/roboto'
+import { Montserrat_800ExtraBold } from '@expo-google-fonts/montserrat'
 
 const { width } = Dimensions.get("window")
 const height = width * 0.6
@@ -17,11 +19,13 @@ const images = {
   image7: require('../assets/image7.jpg'),
 }
 
-export default function Home() {
+export default function Home({ navigation }) {
   const [active, setActive] = useState(0)
 
   let [fontsLoaded, error] = useFonts({
     Montez_400Regular,
+    Roboto_400Regular,
+    Montserrat_800ExtraBold,
   })
 
   if (!fontsLoaded) {
@@ -37,6 +41,10 @@ export default function Home() {
     if (dotActive !== active) {
       setActive(dotActive)
     }
+  }
+
+  const handleOnPress = () => {
+    navigation.navigate("Curriculum")
   }
 
   return (
@@ -75,13 +83,74 @@ export default function Home() {
             <View style={{ paddingHorizontal: 15, alignItems: "center" }}>
               <Text style={styles.keyFacts}>Key facts about our school</Text>
               <Image source={require("../assets/line.png")} />
-              <Title style={{ color: "#4647AB" }}>
+              <Title style={{ color: "#4647AB", fontFamily: "Roboto_400Regular" }}>
                 Nanjing Hankai Academy offers to its' students a truly global experience
                 where their personal dreams are nurtured through inspiration from our
                 excellent teachers and challenged by exceptional learning opportunities.
               </Title>
-              <Image source={require("../assets/line.png")} />
-              <Image source={require("../assets/languages.png")} />
+              <Image source={require("../assets/line.png")} style={{ marginVertical: 10 }} />
+
+              <Image source={require("../assets/languages.png")} style={{ marginVertical: 5 }} />
+              <Title style={{ color: "#4647AB", fontFamily: "Montserrat_800ExtraBold", marginVertical: 10 }}>
+                Languages Taught
+              </Title>
+              <Title style={{ color: "#4647AB", fontFamily: "Roboto_400Regular" }}>
+                English, Spanish, Chinese, Japanese
+              </Title>
+
+              <Image source={require("../assets/englandFlag.png")} style={styles.flag} resizeMode="contain" />
+              <Title style={{ color: "#4647AB", fontFamily: "Montserrat_800ExtraBold", marginVertical: 10 }}>
+                Curriculums
+              </Title>
+              <Title style={{ color: "#4647AB", fontFamily: "Roboto_400Regular", marginBottom: 20 }}>
+                IGCSE, Checkpoint, A Level, LAMDA, NCC, ASDAN
+              </Title>
+
+              <Image source={require("../assets/globe.png")} />
+              <Title style={{ color: "#4647AB", fontFamily: "Montserrat_800ExtraBold", marginVertical: 10 }}>
+                10+ Nationalities
+              </Title>
+
+              <Image source={require("../assets/ts.png")} style={{ marginTop: 40 }} />
+              <Title style={{ color: "#4647AB", fontFamily: "Montserrat_800ExtraBold", marginVertical: 10 }}>
+                Teacher to Students Ratio
+              </Title>
+              <Title style={{ color: "#4647AB", fontFamily: "Roboto_400Regular" }}>
+                5 : 1
+              </Title>
+
+              <Image source={require("../assets/ageGroup.png")} style={{ marginTop: 40 }} />
+              <Title style={{ color: "#4647AB", fontFamily: "Montserrat_800ExtraBold", marginVertical: 10 }}>
+                Age Group
+              </Title>
+              <Title style={{ color: "#4647AB", fontFamily: "Roboto_400Regular" }}>
+                6 - 18
+              </Title>
+
+              <Image source={require("../assets/students.png")} style={{ marginTop: 40 }} />
+              <Title style={{ color: "#4647AB", fontFamily: "Montserrat_800ExtraBold", marginVertical: 10 }}>
+                Students
+              </Title>
+              <Title style={{ color: "#4647AB", fontFamily: "Roboto_400Regular" }}>
+                1500
+              </Title>
+
+              <Image source={require("../assets/fh.png")} style={{ marginTop: 40 }} />
+              <Title style={{ color: "#4647AB", fontFamily: "Montserrat_800ExtraBold", marginVertical: 10 }}>
+                Facilities Highlights
+              </Title>
+
+              <Image source={require("../assets/dbs.png")} style={{ marginTop: 40 }} />
+              <Title style={{ color: "#4647AB", fontFamily: "Montserrat_800ExtraBold", marginVertical: 10 }}>
+                Day and Boarding School
+              </Title>
+
+              <Image source={require("../assets/line.png")} style={{ marginVertical: 10 }} />
+              <TouchableOpacity style={styles.curriculum} onPress={handleOnPress}>
+                <View style={{ alignItems: "center", justifyContent: "center" }}>
+                  <Text style={styles.keyFacts}>Our Curriculum</Text>
+                </View>
+              </TouchableOpacity>
             </View>
           </ScrollView>
         </View>
@@ -96,13 +165,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
   },
   icon: {
-    position: "absolute",
     width: "100%",
-    height: 120
+    height: 120,
+    borderWidth: 1,
+    borderColor: "#C0C0C0",
   },
   carouselView: {
     paddingHorizontal: 15,
-    marginTop: 120,
+    marginTop: 20,
     width: "100%",
     height,
     justifyContent: "space-between"
@@ -137,9 +207,26 @@ const styles = StyleSheet.create({
     margin: 3
   },
   keyFacts: {
-    marginTop: 10,
+    marginTop: 20,
     fontSize: 43,
     color: "#4647AB",
     fontFamily: "Montez_400Regular",
+    alignSelf: "center",
+  },
+  flag: {
+    marginTop: 50,
+    marginVertical: 5,
+    width: 200,
+    height: 100
+  },
+  curriculum: {
+    marginTop: 10,
+    backgroundColor: "#F5F5F5",
+    width: 300,
+    marginBottom: 300,
+    borderWidth: 1,
+    borderColor: "#1E80C1",
+    borderRadius: 50,
+    paddingBottom: 15
   }
 });
